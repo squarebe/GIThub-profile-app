f57edb420f45e4c41f2fbd9d5fdf53cbc221cd93
squarebe <squarebe03@gmail.com>
2015-09-11 10:37:31 +0100

    interacting elements

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -2,7 +2,7 @@ describe('GitHub profile finder', function() {
   it('finds profiles', function() {
     browser.get('http://localhost:8080');
 
-    element(by.model('searchTerm')).sendKeys('spike01');
+    element(by.model('searchCtrl.searchTerm')).sendKeys('spike01');
     element(by.className('btn')).click();
 
     expect(element(by.binding('user.login')).getText()).
